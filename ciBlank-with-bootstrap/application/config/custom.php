9f5bdb89985ce2e7a74a3338fa0a1c5c1b9218c7
Mj Octavio <mj_octaviojr@yahoo.com>
2020-11-10 11:40:41 +1100

    Added ciBlank-with-Bootstrap and ciIon-Auth

---
+++ ciBlank-with-bootstrap/application/config/custom.php
@@ -0,0 +1,5 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['my_app_name']  = 'citutorial';
+$config['my_user_name'] = 'John';
---
+++ ciBlank-with-bootstrap/application/views/templates/footer.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+        </main>
+
+        <!-- Footer -->
+        <footer class="footer mt-auto py-3">
+            <div style="text-align: center">
+                <span class="text-muted">
+                    © <?= date("Y") . " " . $this->config->item('my_app_name') . "."; ?>
+                    Powered by <span><a href="#" target="_blank">MyCompany</a></span>
+                </span>
+            </div>
+        </footer>
+
+    </body>
+
+</html>
+
+
